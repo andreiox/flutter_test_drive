327d566c679b801fbc969b64afd7fc9f6e8aaef0
Andre Macedo <dee.xd@hotmail.com>
2022-05-27 10:21:09 -0300

    feat: add bloc demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test_drive/screens/bloc_demo.dart';
 import 'package:flutter_test_drive/screens/list_transfers.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(CounterApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,6 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: TransferList(),
+      ),
+    );
+  }
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: CounterPage(),
       ),
     );
   }
